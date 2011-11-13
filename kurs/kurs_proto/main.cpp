9c404f5f7d43dfb666e6f8aaa6b6d05217b9c56c
unknown <Pashkoff@.(none)>
2011-11-13 21:43:12 +0200

    libjpeg and cimg

--- kurs/kurs_proto/main.cpp
+++ kurs/kurs_proto/main.cpp
@@ -1,8 +1,19 @@
+#define  XMD_H
+
+#define cimg_use_jpeg
+#include <Cimg.h>
 
 
+using cimg_library::CImg;
+using cimg_library::CImgDisplay;
 
+typedef unsigned char uchar;
 
 int main()
 {
+	CImg<uchar> img("1.jpg");
+	CImgDisplay d(img);
+
+	while(1);
 	return 0;
 }
