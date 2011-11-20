a9729dbcc42b0972d0088d273881b12147dbc874
pashkoff <pashkoff2@gmail.com>
2011-11-20 21:43:17 +0200

    Path to query image is specified in query.in file. query.py tool processes query_maker output and generates res.htm page.

---
+++ kurs/bin/query.py
@@ -0,0 +1,53 @@
+import sys
+import urlparse
+
+argv = sys.argv
+
+if (len(argv) < 4):
+	raise "Not enough arguments"
+
+file_list = argv[1]
+query_in  = argv[2]
+query_out = argv[3]
+res_out   = argv[4]
+
+
+f = open(file_list, 'rt')
+fl = f.read().splitlines()
+f.close()
+
+f = open(query_in, 'rU')
+query = f.readline()
+f.close()
+
+f = open(query_out, 'rU')
+ql = f.read().splitlines()
+f.close()
+
+res = [(fl[int(b[0])],b[1]) for b in [a.split() for a in ql]]
+
+def to_url(path):
+	return urlparse.urlunparse( ('file', '', path, '', '', '') ) 
+
+
+f = open(res_out, 'w')
+f.write(r'<html><head></head><body>')
+
+f.write('<p>Query<br/><img src="')
+f.write(to_url(query))
+f.write('"></p><hr/>')
+
+f.write('<p>Results</p><p><table border="1">')
+for r in res:
+	url = to_url(r[0])
+	print(url)
+
+	f.write('<tr><td><img src="')
+	f.write(url)
+	f.write('"></td><td>')
+	f.write(r[1])
+	f.write('</td></tr>')
+	pass
+
+f.write(r'</table></p></body></html>')
+f.close()
