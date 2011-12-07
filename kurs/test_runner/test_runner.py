ccf059c1c335157f15f9dca3a0ef6c65d0c9f766
pashkoff <pashkoff2@gmail.com>
2011-12-07 21:02:07 +0200

    Working test_runner.

--- kurs/test_runner/test_runner.py
+++ kurs/test_runner/test_runner.py
@@ -1,39 +1,121 @@
 import os
 import subprocess
+import pickle
+
+def check_output(*popenargs, **kwargs):
+    if 'stdout' in kwargs:
+        raise ValueError('stdout argument not allowed, it will be overridden.')
+    process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+    output, unused_err = process.communicate()
+    retcode = process.poll()
+    if retcode:
+        cmd = kwargs.get("args")
+        if cmd is None:
+            cmd = popenargs[0]
+        raise subprocess.CalledProcessError(retcode, cmd, output=output)
+    return output
+
+class CalledProcessError(Exception):
+    def __init__(self, returncode, cmd, output=None):
+        self.returncode = returncode
+        self.cmd = cmd
+        self.output = output
+    def __str__(self):
+        return "Command '%s' returned non-zero exit status %d" % (
+            self.cmd, self.returncode)
+# overwrite CalledProcessError due to `output` keyword might be not available
+subprocess.CalledProcessError = CalledProcessError
 
 
-folders = [r'../../images/50/orig/',
-           r'../../images/50/rot33/',
-           r'../../images/50/rot130/',
-           r'../../images/50/scale1_2/',
-           r'../../images/50/scale2/']
 
-file_list = r'cache/file.list'
-tree_file = r'cache/hikm.tree'
-ivf_file  = r'cache/ivf.file'
+
+###################################
+
+folders = [r'../../images/50/orig',
+           r'../../images/50/rot33',
+           r'../../images/50/rot130',
+           r'../../images/50/scale12',
+           r'../../images/50/scale2']
+
+res_dir = r'res'
+
+file_list  = r'cache/file.list'
+tree_file  = r'cache/hikm.tree'
+ivf_file   = r'cache/ivf.file'
+
+ivf_config  = r'ivf.config'
+tree_config = r'tree.config'
 
 query_maker = r'../kurs/bin/query_maker.exe'
 
+ivf_w_params = [r'none', r'tfidf']
+ivf_n_params = [r'l1', r'l2']
+
+#ivf_w_params = [r'none', r'bin', r'tf', r'tfidf']
+#ivf_n_params = [r'none', r'l0', r'l1', r'l2']
+
+q_d_params = [r'l1', r'l2', r'cos']
+
+def getIvfCmdParams(w, n):
+    return r"IVF_CREATOR_OPTS='-W%(w)s -N%(n)s'" % \
+        {'w': ivf_w_params[w], 'n': ivf_n_params[n] }
+
+def writeIvfConfig(w, n):
+    f = open(ivf_config, 'wt')
+    f.write("weight = %(w)s\nnorm = %(n)s" % \
+        {'w': ivf_w_params[w], 'n': ivf_n_params[n] })
+    f.close()
+    return "IVF_CREATOR_OPTS=-c" + ivf_config
+
+
+#def writeTreeConfig(cl, lv):
+#    f = open(tree_config, 'wt')
+#    f.write("clustres = %(c)s\nleaves = %(l)s" % \
+#        {'c': cl, 'l': lv })
+#    f.close()
+#    return "TREE_CREATOR_OPTS=-c" + tree_config
+
+def writeIvfConfigS(w, n):
+    f = open(ivf_config, 'wt')
+    f.write("weight = %(w)s\nnorm = %(n)s" % \
+        {'w': w, 'n': n })
+    f.close()
+    return "IVF_CREATOR_OPTS=-c" + ivf_config
+
+
+def writeTreeConfigS(cl, lv):
+    f = open(tree_config, 'wt')
+    f.write("clustres = %(c)s\nleaves = %(l)s" % \
+        {'c': cl, 'l': lv })
+    f.close()
+    return "TREE_CREATOR_OPTS=-c" + tree_config
+
 ####################################
-f = open(file_list, 'rt')
-fl = f.read().splitlines()
-f.close()
+fl = []
+try:
+    f = open(file_list, 'rt')
+    fl = f.read().splitlines()
+    f.close()
+except IOError as e:
+    print (e)
+    pass
+
 
 def num2file(num):
     return fl[int(num)]
 
 ####################################
 
-def make_query(query, tree, ivf):
-    cmd = ' '.join([query_maker, '-i', query, '-t', tree, '-v', ivf, '-o --'])
+def make_query(query, tree, ivf, dist):
+    cmd = ' '.join([query_maker, '-i', query, '-t', tree, '-v', ivf, '-D', dist, '-o --'])
     print(cmd)
-    res = subprocess.check_output(cmd)
+    res = check_output(cmd)
     return res.splitlines()
 
 def fname(path):
     return os.path.basename(path)
 
-def test(dir):
+def test(dir, dist):
     files = os.listdir(dir)
 
     res = {}
@@ -41,7 +123,7 @@ def test(dir):
     good_count = 0
 
     for f in files:
-        qres = make_query(dir + f, tree_file, ivf_file)
+        qres = make_query(dir + r'/' + f, tree_file, ivf_file, dist)
         fqres = [(num2file(b[0]),b[1]) for b in [a.split() for a in qres]]
         res[f] = fqres
         g = f == fname(fqres[0][0])
@@ -50,9 +132,86 @@ def test(dir):
             good_count += 1
             pass
 
-        break
         pass
 
     return res, good, good_count
-a,b,c, = test(folders[4])
-print (a,b,c)
+#a,b,c, = test(folders[4])
+#print (a,b,c)
+
+
+def make_base(params = []):
+    prm = ['-j4']
+    prm.extend(params)
+    cmd = ['make', 'base']
+    cmd.extend(prm)
+    print (' '.join(cmd))
+    res = ''
+    try:
+        res = check_output(cmd, stderr = subprocess.STDOUT, shell=True)
+    except subprocess.CalledProcessError as e:
+        print(e)
+        print(e.output)
+        return False
+    print(res)
+    return True
+
+def make_testclean():
+    cmd = ['make', 'testclean']
+    try:
+        res = check_output(cmd, stderr = subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        print(e)
+        return False
+    print(res)
+    return True
+
+
+#make_base([writeIvfConfig(1,1), writeTreeConfig(3,200)])
+
+def run_test(f, d, w, n, cl, lv):
+    print({'f':fname(f), 'd':d, 'w':w, 'n':n, 'cl':cl, 'lv':lv})
+    res_file = "%(f)s__%(d)s__%(w)s__%(n)s__%(cl)d__%(lv)d.res" %\
+        {'f':fname(f), 'd':d, 'w':w, 'n':n, 'cl':cl, 'lv':lv}
+
+    rf = open(res_dir + r'/' + res_file, 'wb')
+
+    make_testclean()
+    make_base([writeIvfConfigS(w,n), writeTreeConfigS(cl, lv)])
+
+    a,b,c = test(f, d)
+    pickle.dump([ [f, d, w, n, cl, lv], [a,b,c] ], rf)
+
+    rf.close()
+    pass
+
+
+
+def run_folder(f):
+    for w in ivf_w_params:
+        for n in ivf_n_params:
+            for d in q_d_params:
+                for cl in [3,5,10]:
+                    for lv in [20, 50, 100]:
+                        run_test(f, d, w, n, cl, lv)
+                        #return
+                        pass
+                    pass
+                pass
+            pass
+        pass
+    return
+
+
+def run_all():
+    for f in folders:
+        run_folder(f)
+        pass
+    return
+
+try:
+    run_all()
+except subprocess.CalledProcessError as e:
+    print(e)
+    print(e.output)
+
+os.system('pause')
